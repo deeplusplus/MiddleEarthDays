590fb6b0c63e516edc5d5f5a5c24551b628b407c
menlo-jarvis <r2dan2d2@gmail.com>
2020-03-30 21:07:04 -0400

    Matt anDan - Add timeline retrospect functionality based on command line arg

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 import json
+import sys
 from me_calendar import Calendar
-from datetime import datetime
-from random import randint
+from datetime import datetime, timezone
 from twitter_client import getTwitterClient
 from dateutil.parser import parse
 
@@ -17,26 +17,30 @@ def main():
 
 def handleNewQueries(client):
     new_mentions = client.GetMentions(count=20, return_json=True)
+    timing_interval = int(sys.argv[1])
 
     for mention in new_mentions:
-        unparsed_tweet_text = mention['text']
-        parsed_tweet_text = ''
-        sender = mention['user']['screen_name']
-        tweet_as_list = unparsed_tweet_text.split()
+        time_since_tweet =  datetime.now(timezone.utc) - parse(mention['created_at'])
 
-        for split_item in tweet_as_list: 
-            if split_item[0] is not '@':
-                parsed_tweet_text += split_item + ' '
-        
-        try:
-            query_date = parse(parsed_tweet_text)
-            print('Parse of date worked')
-            event_text = calendar.getRandomEventOnDate(query_date)
-            print(event_text)
-            if len(event_text) is not 0:
-                client.PostUpdate('@' + sender + ' ' + event_text)
-        except:
-            print('Something went horribly wrong')
+        if time_since_tweet.seconds <= timing_interval:
+            parsed_tweet_text = ''
+            
+            unparsed_tweet_text = mention['text']
+            sender = mention['user']['screen_name']
+
+            tweet_as_list = unparsed_tweet_text.split()
+
+            for split_item in tweet_as_list: 
+                if split_item[0] is not '@':
+                    parsed_tweet_text += split_item + ' '
+            
+            try:
+                query_date = parse(parsed_tweet_text)
+                event_text = calendar.getRandomEventOnDate(query_date)
+                if len(event_text) is not 0:
+                    client.PostUpdate('@' + sender + ' ' + event_text)
+            except:
+                print('Something went horribly wrong')
 
 
 def handleDailyTweet(client):
