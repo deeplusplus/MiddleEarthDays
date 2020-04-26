cf5096edd868056f7a1945f617bf6bad58852126
menlo-jarvis <r2dan2d2@gmail.com>
2020-04-26 16:47:32 -0400

    Stop gap.  Currently handles everything except generating the correct hyperlink to the code text.

--- __main__.py
+++ __main__.py
@@ -15,7 +15,7 @@ def main():
 
 def handleNewQueries(client):
     new_mentions = client.GetMentions(count=20, return_json=True)
-    timing_interval = 300    
+    timing_interval = 60    
 
     for mention in new_mentions:
         time_since_tweet =  datetime.now(timezone.utc) - parse(mention['created_at'])
@@ -34,13 +34,13 @@ def handleNewQueries(client):
             
             try:
                 query_code_number = parsed_tweet_text.strip()
-                code_object = code_retriever.getCodeByNumber(query_code_number)
-                tweet_text = make_tweet_content_from_code_object(code_object)
+                lamc_section = code_retriever.getCodeByNumber(query_code_number)
+                tweet_text = make_tweet_content_from_section_number(lamc_section)
                 client.PostUpdate(tweet_text, in_reply_to_status_id=tweet_id, auto_populate_reply_metadata=True)
             except:
                 print('Something went horribly wrong')
 
-def make_tweet_content_from_code_object(code_object):
+def make_tweet_content_from_section_number(code_object):
     tweet_text  = "LA Municipal Code " + code_object["number"] + "\n" + code_object["Text"] + "\n" + code_object["Link"]
     return tweet_text
 
--- code_retriever.py
+++ code_retriever.py
@@ -17,9 +17,13 @@ class CodeRetriever:
 
     def getCodeByNumber(self, number):
         returned_code = self.CONST_DEFAULT_CODE
+        section_title = ("SEC. " + number + ". ")
 
         for individual_code in self._sections:
-            if ("SEC. " + number + ". ") in individual_code:
-                returned_code = individual_code.strip()
+            if section_title in individual_code:
+                returned_code = { "number": section_title,
+                "Text":  individual_code.strip(),
+                "Link": "https://codelibrary.amlegal.com/codes/los_angeles/latest/lamc/"
+                }
 
         return returned_code
